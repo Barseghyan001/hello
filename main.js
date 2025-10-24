fae7d3a8135a2549e65dd48f05914f9d2276aa55
Hrant <barseghyan085@gmail.com>
2025-10-24 18:18:02 +0400

    d

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const input = document.querySelector('.input');
+const inputback = document.querySelector('.inputback');
+const inputtext = document.querySelector('.text');
 const h1 = document.querySelector('.h1');
 
 
 input.addEventListener('input', () => {
   const color = input.value;
   h1.style.color = color;
+})
+
+inputback.addEventListener('input', () => {
+  const color = inputback.value;
+  document.body.style.background = color;
+})
+
+inputtext.addEventListener('input', () => {
+  const text = inputtext.value;
+  console.log(text);
+  h1.innerText = text;
 })
